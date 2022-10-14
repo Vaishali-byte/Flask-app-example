2b328dcee459f0ad5162b1750ceafdc190eb9096
Vaishali Pawar <vpwork9@gmail.com>
2022-10-14 13:41:07 +0100

    Making a develop branch for second

--- application/routes.py
+++ application/routes.py
@@ -5,13 +5,29 @@ from application import app, db
 from application.models import Todos, Lists
 from application.forms import TodoForm, ListForm
 
-#READ 
+#READ BOTH DATABASES
+#Location of this functionality: ip_address:5000/
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    lists = Lists.query.all()
     todos = Todos.query.all()
-    return render_template('index.html', title="To do List", todos=todos)
+    return render_template('index.html', title="To do List", todos=todos, lists=lists)
 
-#CREATE 
+# CREATE list items 
+@app.route('/addlist', methods=['POST', 'GET'])
+def listadd():
+    form = ListForm() 
+    if form.validate_on_submit(): 
+        lists = Lists(
+            name = form.name.data
+        )
+        db.session.add(lists)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('addlists.html', title="Add a new Task", form=form)
+
+#CREATE todo items
+#Location of this functionality: ip_address:5000/add
 @app.route('/add', methods=['POST','GET'])
 def add():
     # This points to TodoForm
@@ -33,8 +49,30 @@ def add():
     return render_template('add.html', title="Add a new Task", form=form)
 
 #UPDATE 
+@app.route('/update/<int:tid>', methods=['GET', 'POST'])
+def update(tid):
+    form = TodoForm()
+    # Get one tasks from the specified ID
+    tasks = Todos.query.get(tid)
+    # POST method
+    # If the user clicks submit
+    if form.validate_on_submit():
+        # What is put in the form gets ammended to the database
+        tasks.tasks = form.tasks.data
+        # Commit the changes
+        db.session.commit()
+        # Redirect to the url for index function 
+        return redirect(url_for('index'))
+    # Else if the request method is a GET
+    elif request.method == 'GET':
+        # Update the form with whats in the database
+        form.tasks.data = tasks.tasks 
+    # If we go to the url return the template update.html
+    return render_template('update.html', title='Update you task', form=form)
+
 
 #DELETE
+#Location of this functionality: ip_address:5000/delete/1
 @app.route('/delete/<int:tid>')
 def delete(tid):
     # Collecting the task we want to delete based on its id
